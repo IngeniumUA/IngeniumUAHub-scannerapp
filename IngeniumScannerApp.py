8e912957a593c56da6d8ea56f9fac86c5131efe4
kippie <wout.de.smit@hotmail.be>
2023-12-04 20:33:55 +0100

    implemented IngeniumAPIReplica and support for it

--- IngeniumScannerApp.py
+++ IngeniumScannerApp.py
@@ -11,17 +11,82 @@ from PIL import Image
 
 from pyzbar.pyzbar import decode
 
+import random
 import requests
 
 
-resultlist = ["All-In, wout.de.smit@hotmail.be", "All-In, wout.de.smit@hotmail.com"]
-prevresult = ""
-prevevent = ""
+class IngeniumAPIReplica:
+    def __init__(self, instance):
+        self.instance = instance
+        self.AcceptableTokens = []
+        self.AcceptableResets = []
+        self.logindb = {"wout.de.smit@hotmail.be": "RobbieIsMyB", "wout.de.smit@gmail.com": "ILoveSpaghett"}
+        self.evendtdb = {"UUID1": {"event": "All-In", "Validity": "Valid"},
+                         "UUID2": {"event": "All-In", "Validity": "InValid"},
+                         "UUID3": {"event": "All-In", "Validity": "Used"},
+                         "UUID4": {"event": "Schachtenkoningcantus", "Validity": "Valid"}}
+
+    def login(self, user_email, user_id):
+        if user_email in self.logindb:
+            if self.logindb[user_email] == user_id:
+                APItoken = random.randint(100000, 999999)
+                APIReset = random.randint(100000, 999999)
+                self.AcceptableTokens.append(APItoken)
+                self.AcceptableResets.append(APIReset)
+                return APItoken, APIReset
+            else:
+                return "LoginError", 0
+        else:
+            return "LoginError", 0
+
+    def get_validity(self, APIToken, UUID, event):
+        if APIToken in self.AcceptableTokens:
+            if UUID in self.evendtdb:
+                if self.evendtdb[UUID]["event"] == event:
+                    validity = self.evendtdb[UUID]["Validity"]
+                    return validity
+                else:
+                    return "eventError"
+            else:
+                return "UUIDError"
+        else:
+            return "APITokenError"
+
+    def reset_token(self, APIReset):
+        if APIReset in self.AcceptableResets:
+            APItoken = random.randint(100000, 999999)
+            self.AcceptableTokens.append(APItoken)
+            return APItoken
+        else:
+            return "resetError"
+
+    def resettokens(self):
+        self.AcceptableTokens = []
+
+    def resetresets(self):
+        self.AcceptableResets = []
 
 
 class LoginScreen(MDScreen):
+    def login(self):
+        global Token, Reset, API
+        Token, Reset = API.login(self.ids.mail.text, self.ids.passw.text)
+        self.ids.validitylabel.text = "data invalid"
+        if Token != "LoginError":
+            self.ids.validitylabel.text = "data valid"
+        else:
+            self.ids.validitylabel.text = "data invalid"
     pass
 
+    def buttonpress(self):
+        global Token, sm
+        if Token != "LoginError":
+            ScannerAlowed = True
+        else:
+            ScannerAlowed = False
+        sm.transition.direction = "left"
+        sm.current = "scan" if ScannerAlowed else "login"
+        pass
 
 class ScanScreen(MDScreen):
 
@@ -30,14 +95,41 @@ class ScanScreen(MDScreen):
 
     @mainthread
     def got_result(self, result):
-        self.ids.ti.text = str(result)
-        global prevresult, prevevent
-        if self.ids.event.text + ", " + result in resultlist and (result != prevresult or self.ids.event.text != prevevent):
+        # self.ids.ti.text = str(result)
+        global prevresult, prevevent, Token, Reset, API, sm
+        if API.get_validity(Token, result, self.ids.event.text) == "APITokenError" \
+                and (result != prevresult or self.ids.event.text != prevevent):
+            self.ids.ti.text = "Resetting token"
+            self.ids.ti.background_color = 0, 0, 1, 1
+            Token = API.reset_token(Reset)
+            if Token == "resetError":
+                sm.current = "login"
+        elif API.get_validity(Token, result, self.ids.event.text) == "Valid" \
+                and (result != prevresult or self.ids.event.text != prevevent):
             self.ids.ti.background_color = 0, 1, 0, 1
-        elif result != prevresult or self.ids.event.text != prevevent:
+            self.ids.ti.text = "Valid"
+        elif API.get_validity(Token, result, self.ids.event.text) == "InValid" \
+                and (result != prevresult or self.ids.event.text != prevevent):
+            self.ids.ti.background_color = 253/255, 127/255, 57/255, 1
+            self.ids.ti.text = "InValid"
+        elif API.get_validity(Token, result, self.ids.event.text) == "Used" \
+                and (result != prevresult or self.ids.event.text != prevevent):
             self.ids.ti.background_color = 1, 0, 0, 1
+            self.ids.ti.text = "Used"
+        elif API.get_validity(Token, result, self.ids.event.text) == ("eventError" or "UUIDError") \
+                and (result != prevresult or self.ids.event.text != prevevent):
+            self.ids.ti.background_color = 1, 0, 0, 1
+            self.ids.ti.text = "Didn't pay"
         prevresult = result
         prevevent = self.ids.event.text
+
+    def resettokens(self):
+        global API
+        API.resettokens()
+
+    def resetresets(self):
+        global API
+        API.resetresets()
 
 
 class ScanAnalyze(Preview):
@@ -60,6 +152,7 @@ class QRScan(MDApp):
             from android.permissions import request_permissions, Permission
             request_permissions([Permission.WRITE_EXTERNAL_STORAGE, Permission.CAMERA, Permission.RECORD_AUDIO])
 
+        global sm
         sm = MDScreenManager()
         sm.add_widget(LoginScreen(name='login'))
         sm.add_widget(ScanScreen(name='scan'))
@@ -67,4 +160,12 @@ class QRScan(MDApp):
 
 
 if __name__ == '__main__':
+    resultlist = ["All-In, wout.de.smit@hotmail.be", "All-In, wout.de.smit@hotmail.com"]
+    prevresult = ""
+    prevevent = ""
+    Token = 0
+    Reset = 0
+    API = IngeniumAPIReplica(True)
+    sm = 0
+
     QRScan().run()

39d09399afe419e3220f607e6730c4a16d3e6309
kippie <wout.de.smit@hotmail.be>
2023-12-19 12:00:21 +0100

    updated app to use hub api

--- IngeniumScannerApp.py
+++ IngeniumScannerApp.py
@@ -15,83 +15,59 @@ from pyzbar.pyzbar import decode
 import requests
 
 from data_models import PyStaffTransaction
-from hub_api import get_transactions
+from hub_api import get_transactions, authenticate, refresh_token, PyToken
 
 Config.set('graphics', 'resizable', True)
 
 
-def update(APIToken, UUID):
-    requests.put("http://127.0.0.1:8000" + '/api/v1/staff/transaction/update', data={'access_token': APIToken, 'item_id': UUID})
+def update(token, uuid):
+    requests.put("http://127.0.0.1:8000" + '/api/v1/staff/transaction/update', data={'access_token': token,
+                                                                                     'item_id': uuid})
 
 
-def login(user_email, user_id):
-    login = requests.post("http://127.0.0.1:8000" + "/api/v1/auth/token", data={'username': user_email, 'password': user_id})
-    if login.status_code == 200:
-        APIToken = login.json()['access_token']
-        APIReset = login.json()['refresh_token']
-        return APIToken, APIReset
-    else:
-        return "LoginError", 0
-
-
-def get_validity(APIToken, UUID, event):
+def get_validity(token, uuid, event):
     """
 
-    :param APIToken:
-    :param UUID:
+    :param token:
+    :param uuid:
     :param event:
     :return:
     """
-    transactions: list[PyStaffTransaction] = get_transactions(token=APIToken, checkout_id=str(UUID))
+    transactions: list[PyStaffTransaction] = get_transactions(token=token, checkout_id=str(uuid))
     # With get_transactions we fetch a list of PyStaffTransactions
     # For each item in the list: ( example as "for v in transactions:" )
     #   A unique id has been provided in v.interaction.id
     #   Check the validity with v.validity == 'valid' .. or v.validity == 'invalid'
     #   Display product name v.product['name']
 
-    checkout = requests.get("http://127.0.0.1:8000" + "/api/v1/staff/transaction", params={'access_token': APIToken, "limit": 50, "offset": 0, 'checkout_id': UUID})
-    if checkout.status_code != 200:
+    if transactions is None:
         return "APITokenError", 0
-    if checkout.json() == []:
+    if transactions == []:
         return "UUIDError", 0
-    i = 0
-    while i < len(checkout.json()):
-        if checkout.json()[i]["interaction"]["item_name"].lower() == event:
-            item_id = checkout.json()[i]["interaction"]["id"]
-            validity = checkout.json()[i]["valid_policy"]
-            return validity, item_id
-        else:
-            i += 1
+    for transaction in transactions:
+        if transaction.interaction.item_name.lower() == event:
+            return transaction.validity, transaction.interaction.item_id
     return "eventError", 0
-
-
-def reset_token(APIReset):
-    reset = requests.post("http://127.0.0.1:8000" + "/api/v1/auth/refresh", data={'refresh_token': APIReset})
-    if reset.status_code == 200:
-        APIToken = reset.json()['access_token']
-        return APIToken
-    else:
-        return "resetError"
 
 
 class LoginScreen(MDScreen):
     def login(self):
-        global Token, Reset
-        Token, Reset = login(self.ids.mail.text.lower(), self.ids.passw.text)
-        if Token != "LoginError":
-            self.ids.validitylabel.text = ""
-        else:
+        global token
+        token = authenticate(self.ids.mail.text.lower(), self.ids.passw.text)
+        if token is None:
             self.ids.validitylabel.text = "Email or Password incorrect"
-    pass
+        else:
+            self.ids.validitylabel.text = ""
+        pass
 
     def buttonpress(self):
-        global Token, sm
-        if Token != "LoginError":
-            ScannerAlowed = True
+        global token, sm
+        if token is None:
+            scanner_allowed = False
         else:
-            ScannerAlowed = False
+            scanner_allowed = True
         sm.transition.direction = "left"
-        sm.current = "scan" if ScannerAlowed else "login"
+        sm.current = "scan" if scanner_allowed else "login"
         pass
 
 
@@ -103,36 +79,37 @@ class ScanScreen(MDScreen):
     @mainthread
     def got_result(self, result):
         # self.ids.ti.text = str(result)
-        global prevevent, prevresult, Token, Reset, sm
+        global prev_event, prev_result, token, sm
 
-        validity, item_id = get_validity(Token, result, self.ids.event.text.lower())
+        validity, item_id = get_validity(token, result, self.ids.event.text.lower())
         if validity == "APITokenError":
             result = ""
-            Token = reset_token(Reset)
-            if Token == "resetError":
+            token = refresh_token(token)
+            if token is None:
                 sm.transition.direction = "right"
                 sm.current = "login"
             else:
                 sm.transition.direction = "left"
                 sm.current = "token"
-        elif validity == "valid" and (result != prevresult or self.ids.event.text.lower() != prevevent):
+        elif validity == "valid" and (result != prev_result or self.ids.event.text.lower() != prev_event):
             sm.transition.direction = "left"
             sm.current = "valid"
-            update(Token, item_id)
-        elif validity == "invalid" and (result != prevresult or self.ids.event.text.lower() != prevevent):
+            update(token, item_id)
+        elif validity == "invalid" and (result != prev_result or self.ids.event.text.lower() != prev_event):
             sm.transition.direction = "left"
             sm.current = "invalid"
-            update(Token, item_id)
-        elif validity == "consumed" and (result != prevresult or self.ids.event.text.lower() != prevevent):
+            update(token, item_id)
+        elif validity == "consumed" and (result != prev_result or self.ids.event.text.lower() != prev_event):
             sm.transition.direction = "left"
             sm.current = "used"
-        elif validity == ("eventError" or "UUIDError") and (result != prevresult or self.ids.event.text.lower() != prevevent):
+        elif (validity == ("eventError" or "UUIDError") 
+              and (result != prev_result or self.ids.event.text.lower() != prev_event)):
             sm.transition.direction = "left"
             sm.current = "payless"
         else:
             print("ERROR - validity unknown")
-        prevresult = result
-        prevevent = self.ids.event.text.lower()
+        prev_result = result
+        prev_event = self.ids.event.text.lower()
 
 
 class ScanAnalyze(Preview):
@@ -188,10 +165,9 @@ class QRScan(MDApp):
 
 
 if __name__ == '__main__':
-    Token = 0
-    Reset = 0
-    sm = 0
-    prevevent = ""
-    prevresult = ""
+    token: PyToken
+    sm: MDScreenManager
+    prev_event = ""
+    prev_result = ""
 
     QRScan().run()

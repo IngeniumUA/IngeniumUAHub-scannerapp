0f4d6bee21f79b47f22798375f4d9f9774fb52e0
kippie <wout.de.smit@hotmail.be>
2023-12-19 20:44:14 +0100

    updated responses of get_transactions

--- hub_api.py
+++ hub_api.py
@@ -30,7 +30,7 @@ def refresh_token(token: PyToken) -> PyToken:
 def get_transactions(token: PyToken,
                      checkout_id: str | None = None, user_id: str | None = None, item: str | None = None,
                      status: str | None = None, validity: str | int | None = None,
-                     limit: int = 50, offset: int = 0, ordering: str | None = None) -> list[PyStaffTransaction]:
+                     limit: int = 50, offset: int = 0, ordering: str | None = None) -> list[PyStaffTransaction] | str:
     """
     Get list of PyTransaction models by filter parameters, limit and offset, first degree ordering allowed
 
@@ -68,5 +68,9 @@ def get_transactions(token: PyToken,
     if response.status_code == 200:
         response_body: list[dict] = response.json()  # Already correctly parsed as list of dictionaries
         return list(PyStaffTransaction(**value) for value in response_body)
+    elif response.status_code == 401:  # token has expired
+        return "login_invalid"
+    elif response.status_code == 406:  # uuid has invalid form
+        return "uuid_invalid"
     else:
         print(response.json())

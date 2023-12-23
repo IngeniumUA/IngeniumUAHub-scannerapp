1aa73b4848b6cdf0777f3c4d46f2051b03aa7d0a
kippie <wout.de.smit@hotmail.be>
2023-12-23 16:58:19 +0100

    removed autoselect validity in get result as this isn't used anymore

--- app/functions/get_results.py
+++ app/functions/get_results.py
@@ -11,7 +11,6 @@ def get_results(api_token, uuid: str, event):
     """
     transactions: list[PyStaffTransaction] = get_transactions(token=api_token, checkout_id=str(uuid))
     table_data = []
-    transaction_to_save = []
     if transactions == "login_invalid":
         return {"validity": "APITokenError"}
     if transactions == [] or transactions == "uuid_invalid":
@@ -26,29 +25,11 @@ def get_results(api_token, uuid: str, event):
                            "[size=15]" + str(transaction.validity.value) + "[/size]",
                            "[size=15]" + amount + "[/size]",
                            str(transaction.interaction.id)))
-        if transaction.interaction.item_name.lower() == event:
-            transaction_to_save.append(transaction)
-    if transaction_to_save:
-        userdata = get_userdata(transaction_to_save[0].interaction.user_id)
-
-        item_to_return = 0
-        for item_to_return in transaction_to_save:
-            if item_to_return.validity.value != "consumed":
-                break
-
-        return {"validity": item_to_return.validity.value,
-                "table_data": table_data,
-                "email": item_to_return.interaction.user_email,
-                "checkout_status": item_to_return.status.value,
-                "voornaam": userdata["voornaam"],
-                "naam": userdata["achternaam"],
-                "lidstatus": str(userdata["lidstatus"])}
-    else:
-        userdata = get_userdata(transactions[0].interaction.user_id)
-        return {"validity": "eventError",
-                "table_data": table_data,
-                "email": transactions[0].interaction.user_email,
-                "checkout_status": transactions[0].status.value,
-                "voornaam": userdata["voornaam"],
-                "naam": userdata["achternaam"],
-                "lidstatus": str(userdata["lidstatus"])}
+    userdata = get_userdata(transactions[0].interaction.user_id)
+    return {"validity": "eventError",
+            "table_data": table_data,
+            "email": transactions[0].interaction.user_email,
+            "checkout_status": transactions[0].status.value,
+            "voornaam": userdata["voornaam"],
+            "naam": userdata["achternaam"],
+            "lidstatus": str(userdata["lidstatus"])}

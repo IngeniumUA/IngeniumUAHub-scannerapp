a481ad52bd843b60a3690b2513d0d89d7fdafa16
kippie <wout.de.smit@hotmail.be>
2024-01-04 17:35:58 +0100

    readded check for validity instead of always returning event error

--- app/functions/get_results.py
+++ app/functions/get_results.py
@@ -2,15 +2,16 @@ from app.api.data_models import PyStaffTransaction
 from app.api.hub_api import get_transactions, get_userdata
 
 
-def get_results(api_token, uuid: str, event) -> dict:
+def get_results(api_token, uuid: str, event_uuid) -> dict:
     """
     :param api_token:
     :param uuid:
-    :param event:
+    :param event_uuid:
     :return:
     """
     transactions: list[PyStaffTransaction] = get_transactions(token=api_token, checkout_id=str(uuid))
     table_data = []
+    event_tickets = []
     if transactions == "login_invalid":
         return {"validity": "APITokenError"}
     if transactions == [] or transactions == "uuid_invalid":
@@ -25,11 +26,26 @@ def get_results(api_token, uuid: str, event) -> dict:
                            "[size=15]" + str(transaction.validity.value) + "[/size]",
                            "[size=15]" + amount + "[/size]",
                            str(transaction.interaction.id)))
+        if transaction.interaction.item_id == event_uuid:
+            event_tickets.append(transaction)
     userdata = get_userdata(transactions[0].interaction.user_id)
-    return {"validity": "eventError",
-            "table_data": table_data,
-            "email": transactions[0].interaction.user_email,
-            "checkout_status": transactions[0].status.value,
-            "voornaam": userdata["voornaam"],
-            "naam": userdata["achternaam"],
-            "lidstatus": str(userdata["lidstatus"])}
+    i = 0
+    if event_tickets:
+        for i in range(len(event_tickets)):
+            if event_tickets[i].validity.value != "consumed":
+                break
+        return {"validity": event_tickets[i].validity.value,
+                "table_data": table_data,
+                "email": event_tickets[i].interaction.user_email,
+                "checkout_status": event_tickets[i].status.value,
+                "voornaam": userdata["voornaam"],
+                "naam": userdata["achternaam"],
+                "lidstatus": str(userdata["lidstatus"])}
+    else:
+        return {"validity": "eventError",
+                "table_data": table_data,
+                "email": transactions[0].interaction.user_email,
+                "checkout_status": transactions[0].status.value,
+                "voornaam": userdata["voornaam"],
+                "naam": userdata["achternaam"],
+                "lidstatus": str(userdata["lidstatus"])}

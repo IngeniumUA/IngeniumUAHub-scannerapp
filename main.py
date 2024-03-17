91a92a5e8312d97d2e7f273cba47633edcd602fb
kippie <wout.de.smit@hotmail.be>
2024-03-17 22:13:01 +0100

    updated the version number

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from app.functions.variables import variables
 
 Config.set('graphics', 'resizable', True)  # make images and other elements resize when not the right dimensions
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 
 # initiate screen manager, this will allow the app to switch between screens
